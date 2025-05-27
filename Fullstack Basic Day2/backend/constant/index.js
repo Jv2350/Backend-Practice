820dc1bd7584c108781f4003a9679d16e9fac3ec
Jv2350 <tp8375735@gmail.com>
2025-05-27 16:29:44 +0530

    initialize frontend with Vite and React setup, including essential configurations, styles, and assets.

--- Fullstack Basic Day2/backend/constant/index.js
+++ Fullstack Basic Day2/backend/constant/index.js
@@ -2,16 +2,16 @@ export const data = [
   {
     id: 1,
     title: "First Item",
-    content: "This is the content of the first item."
+    content: "This is the content of the first item.",
   },
   {
     id: 2,
     title: "Second Item",
-    content: "This is the content of the second item."
+    content: "This is the content of the second item.",
   },
   {
     id: 3,
     title: "Third Item",
-    content: "This is the content of the third item."
-  }
+    content: "This is the content of the third item.",
+  },
 ];
